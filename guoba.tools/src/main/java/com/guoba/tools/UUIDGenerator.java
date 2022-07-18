e11b83b4734d249f4aa5aede1677093087be4d20
norris-zhang <norriszhang@outlook.com>
2022-07-18 20:32:25 +1200

    UUID Generator

---
+++ guoba.tools/src/main/java/com/guoba/tools/UUIDGenerator.java
@@ -0,0 +1,9 @@
+package com.guoba.tools;
+
+import java.util.UUID;
+
+public class UUIDGenerator {
+    public static void main(String[] args) {
+        System.out.println(UUID.randomUUID().toString());
+    }
+}
